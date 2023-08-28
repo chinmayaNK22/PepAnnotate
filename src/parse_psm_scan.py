38bee355da2420fe3d072f21d08316c39bf1d212
Chinmaya Narayana <58813915+chinmayaNK22@users.noreply.github.com>
2023-08-28 11:38:14 +0530

    Create parse_psm_scan.py

---
+++ src/parse_psm_scan.py
@@ -0,0 +1,62 @@
+from itertools import islice
+import os
+
+def get_header_idx(infile):
+    with open(os.path.join(infile)) as file:
+        for i in islice(file, 0, 1):
+            split_i = i.rstrip().split('\t')
+            
+            try:
+                peptide = split_i.index('"Annotated Sequence"')
+                rawfile = split_i.index('Raw File')
+                scan = split_i.index('Scan')
+                mz = split_i.index('"m/z [Da]"')
+                z = split_i.index('"Charge"')
+                rt = split_i.index('Raw RT (min)')
+
+            except:
+                peptide = split_i.index('Annotated Sequence')
+                rawfile = split_i.index('Raw File')
+                scan = split_i.index('Scan')
+                mz = split_i.index('m/z [Da]')
+                z = split_i.index('Charge')
+                rt = split_i.index('Raw RT (min)')
+                
+            return peptide, rawfile, scan, z, rt, split_i
+
+def psm_rawinfo(infiles):
+    dicts = {}
+    for infile in infiles:
+        a = get_header_idx(os.path.join(infile))
+        with open(os.path.join(infile)) as file:
+            for i in islice(file, 1, None):
+                split_i = i.rstrip().split('\t')
+                pep = split_i[a[0]]
+                rawfile = split_i[a[1]]
+                scan = split_i[a[2]]
+                z = split_i[a[3]]
+                rt = split_i[a[4]]
+            
+                if pep != 'NA':
+                    if pep not in dicts:
+                        dicts[pep] = [rawfile + '@' + scan + '@' + z + '@' + rt]
+                    else:
+                        dicts[pep].append(rawfile + '@' + scan + '@' + z + '@' + rt)
+
+    return dicts
+
+def rawinfo(infile):
+    a = get_header_idx(os.path.join(infile))
+    dicts = {}
+    with open(os.path.join(infile)) as file:
+        for i in islice(file, 1, None):
+            split_i = i.rstrip().split('\t')
+            rawfile = split_i[a[1]]
+            scan = split_i[a[2]]
+            
+            if rawfile + '@' + scan not in dicts:
+                dicts[rawfile + '@' + scan] = [split_i]
+            else:
+                dicts[rawfile + '@' + scan].append(split_i)
+
+    return dicts
